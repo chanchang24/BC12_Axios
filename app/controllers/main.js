b725f517ee179840a7163cd8fe0bde9ad2ac46c3
chanchang24 <83943885+chanchang24@users.noreply.github.com>
2021-07-17 13:03:55 +0700

    axios end

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,27 +1,114 @@
 
 var sanPhamService = new sanPhamService();
-function getEle(id){
+function getEle(id) {
     return document.getElementById(id);
 }
 
 
-var layDanhSachSP = function(){
-    sanPhamService.layDSSP().then(function(result){// result là mảng lấy từ promise trả về
+var layDanhSachSP = function () {
+    sanPhamService.layDSSP().then(function (result) {// result là mảng lấy từ promise trả về
         // console.log(result.data);
         //render Table
         renderTable(result.data);// result.data là 1 cái mảng
-
-    }).catch(function(error){
+        setLocalStorage(result.data);
+    }).catch(function (error) {
         console.log(error);// something was wrong
     })
 }
- layDanhSachSP();
-function renderTable(mangSP){
+layDanhSachSP();
+
+var themSanPham = function () {
+    
+    //lay thong tin nhap tu form
+    var tenSP = getEle('TenSP').value;
+    var gia = getEle('GiaSP').value;
+    var hinhAnh = getEle('HinhSP').value;
+    var moTa = getEle('moTa').value;
+
+    //khoi tao doi tuong sp tu lop doi tuong san pham
+    var sp = new SanPham(tenSP, gia, hinhAnh, moTa);
+
+    //Goi API de luu data xuong csdl(database)
+    sanPhamService.themSP(sp).then(function (result) {
+        layDanhSachSP();
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
+getEle('btnThemSP').addEventListener('click', function () {
+    getEle('formSP').reset();
+    var modalFooter = document.querySelector('.modal-footer');
+    modalFooter.innerHTML = `<button class =" btn btn-success" onclick="themSanPham()"> Thêm sản phẩm</button>`
+})
+
+//Xoa san pham
+var xoaSanPham = function (id) {
+    sanPhamService.xoaSP(id).then(function () {
+        //load lai danh sach san pham sau khi xoa thanh cong
+        layDanhSachSP();
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
+var capNhatSanPham = function (id) {
+    var tenSP = getEle('TenSP').value;
+    var gia = getEle('GiaSP').value;
+    var hinhAnh = getEle('HinhSP').value;
+    var moTa = getEle('moTa').value;
+
+    //khoi tao doi tuong sp tu lop doi tuong san pham
+    var sp = new SanPham(tenSP, gia, hinhAnh, moTa);
+
+    //Cap nhat thong tin moi
+    sanPhamService.capNhatSP(id , sp).then(function(result){
+        layDanhSachSP();
+        //An modal sau khi cap nhat
+        document.querySelector('#myModal .close').click();
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
+
+var xemSanPham = function (id) {
+
+    sanPhamService.xemSP(id).then(function (result) {
+        console.log(result.data);
+        var sp = result.data;
+        console.log(sp.id);
+        //Mở modal
+        getEle('btnThemSP').click();//dom tới button click sẽ mở lên
+        //JQuery
+        // $('#myModal').modal('show');
+        //Đổ data lên form
+        getEle('TenSP').value = sp.tenSP;
+        getEle('GiaSP').value = sp.gia;
+        getEle('HinhSP').value = sp.hinhAnh;
+        getEle('moTa').value = sp.moTa;
+        
+        //Them button cập nhật cho form
+        var modalFooter = document.querySelector('.modal-footer')
+        modalFooter.innerHTML = `<button class =" btn btn-success" onclick="capNhatSanPham('${sp.id}')">Cập nhật</button>`;
+
+
+
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
+
+getEle('ipTimKiem').addEventListener('keyup', function(){
+    var mangSP = getLocalStorage();
+    var chuoiTK = getEle('ipTimKiem').value;
+    var mangTK = sanPhamService.timKiemSP(mangSP,chuoiTK);
+    renderTable(mangTK);
+
+})
+function renderTable(mangSP) {
     var content = '';
-    mangSP.map(function(sp, index){
+    mangSP.map(function (sp, index) {
         content += `
             <tr>
-                <td>${index+1}</td>
+                <td>${index + 1}</td>
                 <td>${sp.tenSP}</td>
                 <td>${sp.gia}</td>
                 <td>
@@ -29,11 +116,20 @@ function renderTable(mangSP){
                 </td>
                 <td>${sp.moTa}</td>
                 <td>
-                    <button class="btn btn-danger">Xoá</button>
-                    <button class="btn btn-success">Xem</button>
+                    <button class="btn btn-danger" onclick="xoaSanPham('${sp.id}')">Xoá</button>
+                    <button class="btn btn-success" onclick="xemSanPham('${sp.id}')">Xem</button>
                 </td>
             </tr>
         `;
     })
     getEle('tblDanhSachSP').innerHTML = content
+}
+function setLocalStorage(dssp) {
+    localStorage.setItem('DSSP', JSON.stringify(dssp));
+}
+function getLocalStorage() {
+    if (localStorage.getItem('DSSP')) {
+        return JSON.parse(localStorage.getItem('DSSP'));
+    }
+
 }
--- app/services/SanPhamService.js
+++ app/services/SanPhamService.js
@@ -1,10 +1,44 @@
-function sanPhamService(){
-    this.layDSSP = function(){
+function sanPhamService() {
+    this.layDSSP = function () {
         //Get : lay du lieu tu server tu phia backend
         // axios tra ve 1 promise
         return axios({
             url: 'https://60eea4afeb4c0a0017bf4558.mockapi.io/products',
             method: 'GET'
         });
+    }
+    this.themSP = function (sp) {//axios cung cap cho 1 cai data
+        //POST : them moi du lieu
+        return axios({
+            url: 'https://60eea4afeb4c0a0017bf4558.mockapi.io/products',
+            method: 'POST',
+            data: sp,//them thuoc tinh data day len csdl
+        })
+    }
+    this.xoaSP = function (id) {// truyền vào cái duy nhất
+        //DELETE: xóa data thông qua id
+        return axios({
+            url: `https://60eea4afeb4c0a0017bf4558.mockapi.io/products/${id}`,
+            method: 'DELETE',
+        });
+    }
+    this.xemSP = function (id) {
+        //GET: lấy data của 1 sản phầm dựa vào id
+        return axios({
+            url: `https://60eea4afeb4c0a0017bf4558.mockapi.io/products/${id}`,
+            method: 'GET',
+        })
+    }
+    this.capNhatSP = function (id,sp) {
+        return axios({
+            url:`https://60eea4afeb4c0a0017bf4558.mockapi.io/products/${id}`,
+            method: 'PUT',
+            data : sp,
+        })
+    }
+    this.timKiemSP = function(dssp, chuoiTK){
+        return dssp.filter(function(sp){
+            return sp.tenSP.toLowerCase().indexOf(chuoiTK.toLowerCase()) !== -1;// khác -1 hoặc index
+        })
     }
 }
